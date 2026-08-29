fe9c5ad311539caf0be5320c3b612cc9b797176b
Rhian-Dias <rhian.t.s.dias@gmail.com>
2026-08-29 18:55:24 -0300

    Update sistema_caixa_eletronico_aula.py

--- sistema_caixa_eletronico_aula.py
+++ sistema_caixa_eletronico_aula.py
@@ -1,56 +1,60 @@
-saldo = 1500
-escolha = 0
-
 def consulta_saldo(saldo):
-    print("O saldo é: ", saldo)
+    print("Seu saldo é: ", saldo)
 
-def realiza_saque(valor_saque):
+def realiza_saque(saldo):
     valor_saque = float(input("Entre com o valor de saque: "))
-
-    if valor_saque <=0:
+    
+    if valor_saque <= 0:
         print("Valor inválido!")
-
     elif valor_saque > saldo:
-        print("Impossível de realizar a operação")
+        print("Saldo insuficiente!")
     else:
-        print("Saque realizado com sucesso")
         saldo = saldo - valor_saque
+        print("Saque realizado com sucesso!")
+
         return saldo
-        
-def realiza_deposito():
-    valor_deposito = float(input("Entre com o valor de saque: "))
+
+def realiza_deposito(saldo):
+    valor_deposito = float(input("Entre com o valor a ser depositado!"))
+    
     if valor_deposito > 0:
         saldo = saldo + valor_deposito
+
         return saldo
     else:
-        print("Operação inválida. Valor de deposito incorreto.")
+        print("Valor de depósito incorreto")
 
 def sistema_caixa():
-saldo = 1500
-escolha = 0
+    saldo = 1500 
 
-while escolha != 0:
-    print("\n")
-    print("1 - Consultar saldo")
-    print("2 - Sacar")
-    print("3 - Depositar")
-    print("0 - Encerrando o sistema")
+    escolha = 1
 
-    escolha = int(input("Escolha uma opção do menu: "))
+    while escolha != 0:
+        print("1 - Consultar saldo")
+        print("2 - Sacar")
+        print("3 - Depositar")
+        print("0 - Sair")
 
-    if escolha == 1:
-       consulta_saldo(saldo)
+        escolha = (input("Entre com uma opção: "))
 
-    elif escolha == 2:
-       saldo = realiza_saque(saldo)
+        if escolha == 1:
+            consulta_saldo(saldo)
 
-    elif escolha == 3:
-         saldo = realiza_deposito(saldo)
+        elif escolha == 2:
+            saldo = realiza_saque(saldo)
+            
+        elif escolha == 3:
+            saldo = realiza_deposito(saldo)
+        
+        elif escolha == 0:
+            print("Operação finalizada!")
 
-    elif escolha == 0:
-        print("Encerrando o sistema")
-    
-    else:
-        print("Opção incorreta. Tenta novamente.")
+        else:
+            print("Opção incorreta. Tente Novamente!!")
 
 sistema_caixa()
+
+
+
+
+
