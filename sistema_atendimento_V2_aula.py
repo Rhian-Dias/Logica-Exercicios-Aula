6f4a1ac928e588660db812b2988a00d521835a99
Rhian-Dias <rhian.t.s.dias@gmail.com>
2026-08-29 18:57:51 -0300

    Update sistema_atendimento_V2_aula.py

--- sistema_atendimento_V2_aula.py
+++ sistema_atendimento_V2_aula.py
@@ -1,32 +1,34 @@
-escolha = 4
-normal = 0
-prioridade = 0
+def gerar_senha(tipo,numero):
+     return f"{tipo}{numero:03d}"
 
-def gerar_senha(tipo, numero):
-    return f"{tipo}{numero:03d}"
+def sistema_de_senhas():  
+    normal = 0
+    prioridade = 0
+    escolha = 4
 
-def sistema_senhas():
     while escolha != 3:
-        print("1 - Atendimento normal")
-        print("2 - Atendimento prioritário")
-        print("3 - Encerrar atendimento")
-        
+        print("1 - Atendimento Normal")
+        print("2 - Atendimento Prioritário")
+        print("3 - Encerrar Atendimento")
+
         escolha = int(input("Entre com uma opção: "))
 
         if escolha == 1:
-            normal = normal + 1
-            senha = gerar_senha("N", normal)
-            print("A senha gerada é: ", senha)
+            normal = normal+1
+            senha = gerar_senha("N",normal)
 
-        elif escolha == 2:
+            print("Senha gerada é: ", senha)
+
+        elif escolha == 2: 
             prioridade = prioridade + 1
-            senha = gerar_senha("P", prioridade)
-            print("A senha gerada é: ", senha)
-        
-        elif escolha == 3:
-            print("Encerrando o sistema.")
-        
-        else:
-            print("Opção inválida. Digite uma opção válida.")
+            senha = gerar_senha("P",prioridade)
 
-sistema_senhas()
+            print("Senha gerada é: ", senha)
+
+        elif escolha == 3:
+            print("Sistema Encerrado")
+
+        else: 
+            print("Opção inválida! ")
+
+sistema_de_senhas()
