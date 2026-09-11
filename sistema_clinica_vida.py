7a9fc72caa32f8acbd81a3d148def1726f1c585b
Rhian-Dias <rhian.t.s.dias@gmail.com>
2026-09-11 16:47:10 -0300

    Add files via upload

---
+++ sistema_clinica_vida.py
@@ -0,0 +1,57 @@
+"""
+SISTEMA CLINICA
+FUNCIONALIDADES:
+    PACIENTES
+    - cadastro de pacientes
+    - consulta de pacientes
+
+    MEDICOS
+    - controle de médicos
+    - especialidade dos médicos
+    
+    CONSULTAS
+    - agendamento das consultas
+    - cancelamento das consultas
+    - remarcação das consultas
+
+    - fila de atendimento
+        - prioridade
+        - emergencia
+        
+"""
+"""CLINICA SETORES - HEMATOLOGIA:
+    - Clinico geral
+    - hematologia
+    - pneumologia
+    - nefrologia
+    - cardiologista
+
+cadastro de pacientes:
+- Nome 
+- Contato
+- Data de nascimento --> idade
+- possíveis comorbidades
+
+**plano de saude 
+*situação do paciente --> sistema que responde baseado no cadastro
+
+
+consulta de pacientes:
+login:
+- nome/username AND
+- senha
+    --> baseado na resposta, consulta cadastro
+
+controle de medicos da clinica:
+- nome
+- especialidade
+- disponibilidade
+
+CONSULTAS --> AGENDAMENTO, CANCELAMENTO, REMARCAÇÃO E RETORNO (POS-AGENDAMENTO)
+1. AGENDAMENTO
+    - Se o paciente tem cadastro
+    - Se há medico disponivel
+        - Se há dia/hora disponivel com X especialidade
+        - 
+
+"""
